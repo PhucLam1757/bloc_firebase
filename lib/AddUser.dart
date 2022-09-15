969eae69602c501ba82997bc2b85b62cd7f38ca2
Duy Le <lehongduy.bk@gmail.com>
2022-09-15 20:26:59 +0700

    update Firebase Firestore Database

--- lib/AddUser.dart
+++ lib/AddUser.dart
@@ -10,28 +10,53 @@ class AddUser extends StatelessWidget {
 
   AddUser(this.fullName, this.company, this.age);
 
+  late CollectionReference users;
+
   @override
   Widget build(BuildContext context) {
     // Create a CollectionReference called users that references the firestore collection
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
 
-    Future<void> addUser() {
-      // Call the user's CollectionReference to add a new user
-      return users
-          .add({
-        'full_name': fullName, // John Doe
-        'company': company, // Stokes and Sons
-        'age': age // 42
-      })
-          .then((value) => print("User Added"))
-          .catchError((error) => print("Failed to add user: $error"));
-    }
+    users = FirebaseFirestore.instance.collection('users');
 
-    return TextButton(
-      onPressed: addUser,
-      child: Text(
-        "Add User",
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: addUser,
+              child: Text(
+                "Add User",
+              ),
+            ),
+            TextButton(
+              onPressed: removeUser,
+              child: Text(
+                "Remove User",
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> removeUser() {
+    // Call the user's CollectionReference to add a new user
+    return users.doc("0hOGzO12wGzBHJfeLd0Y").delete().then(
+          (doc) => print("Document deleted"),
+          onError: (e) => print("Error updating document $e"),
+        );
+  }
+
+  Future<void> addUser() {
+    // Call the user's CollectionReference to add a new user
+    return users
+        .add({
+          'full_name': fullName, // John Doe
+          'company': company, // Stokes and Sons
+          'age': age // 42
+        })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
   }
 }
